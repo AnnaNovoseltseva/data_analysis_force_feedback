b3d54392b7977515427b4c0a0bc54502d84f2a1e
Anna Novoseltseva <anovoseltseva@wpi.edu>
2018-03-28 01:52:12 -0400

    add data fiting and saving

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 import numpy as np
-import statsmodels.api as smapi
-from statsmodels.formula.api import ols
-import statsmodels.graphics as smgraphics
 
-def reject_outliers2(data, m=2):
-    return data[abs(data - np.mean(data)) < m * np.std(data)]
-
-
-def reject_outliers(data, m=2.):
-    d = np.abs(data - np.median(data))
-    mdev = np.median(d)
-    s = d / mdev if mdev else 0.
-    return data[s < m]
 
 def fit_data_without_outliers(x, y):
-    # Make fit for x data
-    regression = ols("data ~ x", data=dict(data=y, x=x)).fit()
-    # Find outliers #
-    test = regression.outlier_test()
-    outliers = ((y[j], x[j]) for j, t in enumerate(test.icol(2)) if t < 0.5)
-    print 'Outliers: ', list(outliers)
-    # Figure #
-    figure = smgraphics.regressionplots.plot_fit(regression, 1)
-    # Add line #
-    smgraphics.regressionplots.abline_plot(model_results=regression, ax=figure.axes[0])
+    return np.polyfit(x, y, 1)
 
 
+arr_a_x = []
+arr_b_x = []
+arr_a_y = []
+arr_b_y = []
 
 if __name__ == '__main__':
-    # load data in array
-    positions = np.loadtxt('data/positions.txt')
-    for i in range(1, 2):
 
-        position = positions[i-1]
+    positions = np.loadtxt('data/positions.txt')
+    for i in range(1, 36):
+
         # 1st and 2nd column data from file are x-direction data
         # 3rd and 4th column are y-direction data
         data_x = np.loadtxt('data/{}.txt'.format(i), usecols=(0, 1))
@@ -43,9 +25,36 @@ if __name__ == '__main__':
         force_y = data_y[:, 0]
         adc_y = data_y[:, 1]
 
-        fit_data_without_outliers(adc_x, force_x)
-
         # Find parameters for linear equation from data fitting
-        [x_a, x_b] = np.polyfit(adc_x, force_x, 1)
-        [y_a, y_b] = np.polyfit(adc_y, force_y, 1)
+        a_x, b_x = fit_data_without_outliers(adc_x, force_x)
+        a_y, b_y = fit_data_without_outliers(adc_y, force_y)
+
+        # write data in array
+        arr_a_x.append(a_x)
+        arr_b_x.append(b_x)
+        arr_a_y.append(a_y)
+        arr_b_y.append(b_y)
+
+    file_x = open('data_analysis_results_x.txt', 'a')
+    # loop through each item in the list and write it to the output file
+    for (pos, a, b) \
+            in zip(positions, arr_a_x, arr_b_x):
+        file_x.write('{} {} {} \n'.format(str(pos), str(a), str(b)))
+    file_x.write('\n \n')
+    file_x.close()
+
+    file_y = open('data_analysis_results_y.txt', 'a')
+    # loop through each item in the list and write it to the output file
+    for (pos, a, b) \
+            in zip(positions, arr_a_y, arr_b_y):
+        file_y.write('{} {} {} \n'.format(str(pos), str(a), str(b)))
+    file_y.write('\n \n')
+    file_y.close()
+
+
+
+
+
+
+
 
